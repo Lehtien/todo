91323a6fa1870e03a83f40fc37d57472bcb69863
lehti <leaf.mmo@gmail.com>
2024-10-10 20:21:08 +0900

    pc layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
   };
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 max-w-[500px]">
       <h1 className="text-2xl font-bold mb-4 text-center">ToDo App</h1>
       <div className="text-center">
         <TodoForm onAdd={addTodo} />
